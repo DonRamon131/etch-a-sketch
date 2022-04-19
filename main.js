a04ee545e71e83770f56dbf2b4e02bde6f0ffa58
steven <stevenescobar131@gmail.com>
2022-04-18 19:17:35 -0500

    working on project

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 let main_div = document.getElementById('mainDiv');
 
-for (let i = 0; i <16; i++) {
-    for (let j = 0; j < 15; j++) {
-        main_div.innerHTML += "<div>+</div>";
-        }
-    main_div.innerHTML += "<div>*</div>";
+for (let i = 0; i <256; i++) {
+    main_div.innerHTML += "<div>$</div>";
 }

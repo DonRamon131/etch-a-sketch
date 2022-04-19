35990d1bcd2c4dd8f139d0be62b838cc263edabe
steven <stevenescobar131@gmail.com>
2022-04-18 20:17:12 -0500

    hover feature added

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 let main_div = document.getElementById('mainDiv');
 
 for (let i = 0; i <256; i++) {
-    main_div.innerHTML += "<div></div>";
+    main_div.innerHTML += "<div class=dividio> </div>";
 }
+
+/*let collection = document.getElementsByClassName("dividio");*/
+
+main_div.addEventListener('mouseover', function(event) {
+        event.target.style.backgroundColor = "black";
+});
